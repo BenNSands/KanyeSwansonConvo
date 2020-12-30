adb6d63e604c22a122e94f5f160125bdf45641d0
Ben Sands <m3gap0ky0@gmail.com>
2020-12-29 18:41:41 -0700

    init commit

---
+++ KanyeWestAPI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWestAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Kanye: ");
+                Console.WriteLine(QuoteGenerator.GetKanye());
+                Console.WriteLine();
+                Console.WriteLine("Swanson:");
+                Console.WriteLine(QuoteGenerator.GetSwanson());
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
---
+++ KanyeWestAPI/QuoteGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+
+namespace KanyeWestAPI
+{
+    class QuoteGenerator
+    {
+        public static string GetKanye()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            return kanyeQuote;
+        }
+
+        public static string GetSwanson()
+        {
+            var client = new HttpClient();
+            var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var swansonResponse = client.GetStringAsync(swansonURL).Result;
+            //Console.WriteLine(kanyeResponse);
+
+            var swansonQuote = JArray.Parse(swansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            //var swansonQuote = JObject.Parse(swansonResponse).GetValue("quote").ToString();
+            return swansonQuote;
+        }
+    }
+}
